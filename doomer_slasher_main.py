f2a0cbf3b2486d0a613326dc47d19d6d3bd95898
budes <samuelbrandaoandrade@gmail.com>
2024-09-28 23:39:17 -0300

    tentei implementar o movimento diagonal com hold, mas tá dando errado e n consigo entender pq, só com tapping que vai

--- doomer_slasher_main.py
+++ doomer_slasher_main.py
@@ -2,12 +2,14 @@
 # me limitar ao que foi ensinado nas aulas de prog1 da ufcg
 
 import curses
+import time
 
 borders = ["▀", "▄", "▟", "▜", "▙", "▛", "▐", "▌"]
 
 
 def main(scr):
     scr.clear()
+    scr.nodelay(True)
 
     max_width = curses.COLS
     max_height = curses.LINES
@@ -19,29 +21,36 @@ def main(scr):
     scr.addch(*char_coords, character)
     scr.refresh()
 
+    active_keys = set()
+
     # main loop
     while True:
-        key = scr.getkey()
+        time.sleep(0.001)
 
-        if key == 'KEY_UP':
-            move_character(char_coords, "u")
-        elif key == 'KEY_DOWN':
-            move_character(char_coords, "d")
-        elif key == 'KEY_LEFT':
-            move_character(char_coords, "l")
-        elif key == 'KEY_RIGHT':
-            move_character(char_coords, "r")
-        elif key == 'q':  # quit the loop if 'q' is pressed
+        try:
+            key = scr.getkey()
+            active_keys.add(key)
+        except: key = ""
+
+        if 'KEY_UP' in active_keys:    move_character(char_coords, "u")
+        if 'KEY_DOWN' in active_keys:  move_character(char_coords, "d")
+        if 'KEY_LEFT' in active_keys:  move_character(char_coords, "l")
+        if 'KEY_RIGHT' in active_keys: move_character(char_coords, "r")
+        if key == 'q':  # quit the loop if 'q' is pressed
             break
 
         scr.clear()
         scr.addch(*char_coords, character)
 
+        if key not in ["KEY_UP", "KEY_DOWN", "KEY_LEFT", "KEY_RIGHT"]:
+            active_keys.clear()
+
 # Character handling
 def move_character(coords, side):
     if side in "lr":
-        coords[1] += 1 if side == "r" else -1
+        coords[1] += 2 if side == "r" else -2
     elif side in "ud":
-        coords[0] += 1 if side == "d" else -1
+        coords[0] += 2 if side == "d" else -2
+
 
 curses.wrapper(main)
