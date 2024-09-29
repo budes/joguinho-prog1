85c9e98de8a171e060f4b15d40dc4c28955e2395
budes <samuelbrandaoandrade@gmail.com>
2024-09-29 11:48:47 -0300

    movimento melhorado, um pouco de delay/buffer p facilitar o input

--- doomer_slasher_main.py
+++ doomer_slasher_main.py
@@ -15,11 +15,13 @@ borders = ["▀", "▄", "▟", "▜", "▙", "▛", "▐", "▌"]
 def main(scr):
     scr.clear()
     scr.nodelay(True)
+    curses.curs_set(0)
 
     max_width = curses.COLS
     max_height = curses.LINES
 
     character = "█"
+
     # coordinates system is based in y, x -> curses.addch(y, x)
     char_coords = [max_height//2, max_width//2]
 
@@ -27,28 +29,45 @@ def main(scr):
     scr.refresh()
 
     active_keys = set()
+    count_not_active_buffer = 0
 
     # main loop
     while True:
-        time.sleep(1/60)
+        # frame rate setup
+        time.sleep(1/30)
 
         try:
             key = scr.getkey()
             active_keys.add(key)
         except: key = ""
 
-        if 'KEY_UP' in active_keys:    move_character(char_coords, "u")
-        if 'KEY_DOWN' in active_keys:  move_character(char_coords, "d")
-        if 'KEY_LEFT' in active_keys:  move_character(char_coords, "l")
-        if 'KEY_RIGHT' in active_keys: move_character(char_coords, "r")
+        if 'KEY_UP' in active_keys:
+            if " " in active_keys: dash(char_coords, "u")
+            else: move_character(char_coords, "u")
+        if 'KEY_DOWN' in active_keys:
+            if " " in active_keys: dash(char_coords, "d")
+            else: move_character(char_coords, "d")
+        if 'KEY_LEFT' in active_keys:
+            if " " in active_keys: dash(char_coords, "l")
+            else: move_character(char_coords, "l")
+        if 'KEY_RIGHT' in active_keys:
+            if " " in active_keys: dash(char_coords, "r")
+            else: move_character(char_coords, "r")
+
         if key == 'q':  # quit the loop if 'q' is pressed
             break
 
         scr.clear()
         scr.addch(*char_coords, character)
+        scr.addstr(10, 10, str(list(active_keys)))
 
-        if key not in ["KEY_UP", "KEY_DOWN", "KEY_LEFT", "KEY_RIGHT"]:
-            active_keys.clear()
+        if key != " " and key not in ["KEY_UP", "KEY_DOWN", "KEY_LEFT", "KEY_RIGHT"]:
+            # This countdown makes the input easier to put
+            # The downside is that it makes the step longer
+            count_not_active_buffer += 1
 
+            if count_not_active_buffer == 2:
+                count_not_active_buffer = 0
+                active_keys.clear()
 
 curses.wrapper(main)
--- scripts/character.py
+++ scripts/character.py
@@ -1,5 +1,24 @@
 def move_character(coords, side, step=2, mod=2):
+    """
+    Take cares of the movement:
+    u -> Up | d -> Down | r -> Right | l -> Left
+
+    The mod value is to take care of the fact that the line height is
+    greater than the line width.
+    """
+
     if side in "lr":
         coords[1] += step * mod if side == "r" else -step * mod
     elif side in "ud":
         coords[0] += step if side == "d" else -step
+
+
+def dash(coords, side, step=2, mod=2):
+    """
+    In order to not be that complex of a function, it just uses
+    the default movement function with a modifier to the step value.
+    """
+
+    move_character(coords, side, step*mod)
+
+
