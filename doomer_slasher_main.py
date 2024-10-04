8557dd519116d2ad73bf31efe40fc6103076e8dd
Samuel Brandão <74151920+budes@users.noreply.github.com>
2024-10-04 15:13:40 -0300

    Update doomer_slasher_main.py

--- doomer_slasher_main.py
+++ doomer_slasher_main.py
@@ -5,6 +5,8 @@ import sys
 sys.path.insert(1, "./scripts/")
 
 from character import *
+from boss import *
+from movement import *
 from map import *
 
 import curses
@@ -22,23 +24,29 @@ def main(scr):
     max_height = curses.LINES
 
     character = "█"
+    enemy = "▒"
 
     # coordinates system is based in y, x -> curses.addch(y, x)
     char_coords = [max_height//2, max_width//2]
+    boss_coords = [max_height//2 - 10, max_width//2]
 
+    scr.addch(*boss_coords, character)
     scr.addch(*char_coords, character)
+
     scr.refresh()
 
+    directions = [-1, -1]
     active_keys = set()
     count_not_active_buffer = 0
 
+    attacks = []
     obstacles = generate_obstacles(max_height, max_width)
 
     # main loop
     while True:
         # frame rate setup
-        time.sleep(1/30)
-        
+        time.sleep(1/60)
+
         try:
             key = scr.getkey()
             active_keys.add(key)
@@ -61,10 +69,32 @@ def main(scr):
             break
 
         scr.clear()
+
+        # Boss_movement update and if it had touched any wall
+        directions, side_inversion = boss_movement(boss_coords, max_width, max_height, directions)
+
+        # Updates the current attacks
+        for index in range(len(attacks)-1, -1, -1):
+            move = attacks[index]
+            if move[0] == "side" and move[2] not in [-1, None]:
+                attacks[index][2] = side_attack(scr, character, move[1], max_width, max_height, move[2])
+            else:
+                attacks.pop(index)
+        # If it notices any wall touching, it will tell the side it inverted at
+        # And use it in the side attack function
+        if side_inversion:
+            if side_inversion in "lu": curr_pos = 2
+            elif side_inversion == "r": curr_pos = max_width - 3
+            else: curr_pos = max_height - 3
+
+            attacks.append(["side", side_inversion, side_attack(scr, character, side_inversion, max_width, max_height, curr_pos)])
+
+
+        scr.addstr(10, 10, str(attacks))
+        scr.addch(*boss_coords, enemy)
         scr.border(0)
         game_map(scr, obstacles, borders)
         scr.addch(*char_coords, character)
-        scr.addstr(10, 10, str(list(active_keys)))
 
         if key != " " and key not in ["KEY_UP", "KEY_DOWN", "KEY_LEFT", "KEY_RIGHT"]:
             # This countdown makes the input easier to put
