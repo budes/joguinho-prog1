5f4f1bf0dfe7e151493b35e11e1146fd3ab28f94
Samuel Brandão <74151920+budes@users.noreply.github.com>
2024-10-04 15:09:14 -0300

    Update character.py

--- scripts/character.py
+++ scripts/character.py
@@ -1,26 +1,3 @@
-def move_character(coords, side, can_move, step=2, mod=2):
-    """
-    Take cares of the movement:
-    u -> Up | d -> Down | r -> Right | l -> Left
-
-    The mod value is to take care of the fact that the line height is
-    greater than the line width.
-    """
-    if can_move:
-        if side in "lr":
-            coords[1] += step * mod if side == "r" else -step * mod
-        elif side in "ud":
-            coords[0] += step if side == "d" else -step
-
-
-def dash(coords, side, step=2, mod=2):
-    """
-    In order to not be that complex of a function, it just uses
-    the default movement function with a modifier to the step value.
-    """
-
-    move_character(coords, side, step*mod)
-
 def attack_animation(scr, character_coordinates, side, symbol="#", distance=3, width=1):
     """
     The system of side will be the same as the movement parts
