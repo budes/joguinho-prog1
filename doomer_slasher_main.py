f11434b2eddfb2e9e61d6c7ae3ab15fde7109a25
budes <samuelbrandaoandrade@gmail.com>
2024-10-07 18:06:42 -0300

    atualizacao movimentacão

--- doomer_slasher_main.py
+++ doomer_slasher_main.py
@@ -16,6 +16,8 @@ borders = ["▀", "▄", "▟", "▜", "▙", "▛", "▐", "▌"]
 
 def main(scr):
     scr.clear()
+
+    FPS = 60
     scr.nodelay(True)
     curses.curs_set(0)
 
@@ -23,9 +25,9 @@ def main(scr):
     max_height = curses.LINES
 
     character = "█"
-    character_step = 1
+    character_step = 4
     character_mod = 2
-    dash_multiplier = 2
+    dash_multiplier = 3
 
     boss = "▒"
     boss_step = 1
@@ -51,7 +53,7 @@ def main(scr):
     # main loop
     while not terminate:
         # frame rate setup
-        time.sleep(1/30)
+        time.sleep(1/FPS)
 
         try:
             key = scr.getkey()
@@ -59,17 +61,17 @@ def main(scr):
         except: key = ""
 
         if 'KEY_UP' in active_keys:
-            if " " in active_keys and can_move(max_height, char_coords[0], -character_step * 2): dash(char_coords, "u")
-            elif can_move(max_height, char_coords[0], -character_step): move_character(char_coords, "u")
+            if " " in active_keys and can_move(max_height, char_coords[0], -character_step * dash_multiplier): dash(char_coords, "u", character_step, dash_multiplier)
+            elif can_move(max_height, char_coords[0], -character_step): move_character(char_coords, "u", character_step, character_mod)
         if 'KEY_DOWN' in active_keys:
-            if " " in active_keys and can_move(max_height, char_coords[0], character_step * 2): dash(char_coords, "d")
-            elif can_move(max_height, char_coords[0], character_step): move_character(char_coords, "d")
+            if " " in active_keys and can_move(max_height, char_coords[0], character_step * dash_multiplier): dash(char_coords, "d", character_step, dash_multiplier)
+            elif can_move(max_height, char_coords[0], character_step): move_character(char_coords, "d", character_step, character_mod)
         if 'KEY_LEFT' in active_keys:
-            if " " in active_keys and can_move(max_width, char_coords[1], -character_step * character_mod * 2): dash(char_coords, "l")
-            elif can_move(max_width, char_coords[1], -character_step * character_mod): move_character(char_coords, "l")
+            if " " in active_keys and can_move(max_width, char_coords[1], -character_step * character_mod * 2): dash(char_coords, "l", character_step, dash_multiplier)
+            elif can_move(max_width, char_coords[1], -character_step * character_mod): move_character(char_coords, "l", character_step, character_mod)
         if 'KEY_RIGHT' in active_keys:
-            if " " in active_keys and can_move(max_width, char_coords[1], character_step * character_mod * 2): dash(char_coords, "r")
-            elif can_move(max_width, char_coords[1], character_step * character_mod): move_character(char_coords, "r")
+            if " " in active_keys and can_move(max_width, char_coords[1], character_step * character_mod * 2): dash(char_coords, "r", character_step, dash_multiplier)
+            elif can_move(max_width, char_coords[1], character_step * character_mod): move_character(char_coords, "r", character_step, character_mod)
 
         if key == 'q':  # quit the loop if 'q' is pressed
             terminate = True
@@ -100,19 +102,19 @@ def main(scr):
             attacks.append(["side", side_inversion, side_attack(scr, char_coords, side_inversion, max_width, max_height, curr_pos)])
 
 
-        scr.addstr(10, 10, str(attacks))
+        scr.addstr(10, 10, str(active_keys))
         scr.addch(*boss_coords, boss)
         scr.border(0)
         game_map(scr, obstacles, borders)
         scr.addch(*char_coords, character)
 
-        if key != " " and key not in ["KEY_UP", "KEY_DOWN", "KEY_LEFT", "KEY_RIGHT"]:
-            # This countdown makes the input easier to put
-            # The downside is that it makes the step longer
+        if " " in active_keys and key in ["KEY_UP", "KEY_DOWN", "KEY_LEFT", "KEY_RIGHT"]:
+            count_not_active_buffer = 0
+            active_keys.clear()
+        elif " " in active_keys and not key in ["KEY_UP", "KEY_DOWN", "KEY_LEFT", "KEY_RIGHT"]:
             count_not_active_buffer += 1
-
-            if count_not_active_buffer == 4:
-                count_not_active_buffer = 0
-                active_keys.clear()
+            if count_not_active_buffer > FPS//2: active_keys.clear()
+        else:
+            active_keys.clear()
 
 curses.wrapper(main)
